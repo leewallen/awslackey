eac255f8aa923086e5df14fae82c943b9c93e521
lwallen <lwallen@gettyimages.com>
2017-08-09 23:04:12 -0700

    adding dependency for aws-sdk, and removing comment with link since it messed up bot help

--- scripts/describe-instance.js
+++ scripts/describe-instance.js
@@ -1,37 +1,38 @@
 // Commands:
 //   hubot describe instance [instance_id] - Shows instance details.
 //
-// https://raw.githubusercontent.com/andrewpuch/hubot_slack_example/master/describe-instance.js
-//----------------------------------------------------------------------------------------------
 
 var config  = require(__dirname + '/../config.json'),
-	promise = require('bluebird'),
-	aws     = require('aws-sdk'),
-	ec2     = new aws.EC2(config);
+    promise = require('bluebird'),
+    aws     = require('aws-sdk'),
+    ec2     = new aws.EC2(config);
 
 module.exports = function(robot) {
-	robot.response(/describe instance ([-\a-zA-Z0-9]+)/i, function(msg) {
-		var instance = msg.match[1].toLowerCase(),
-			message  = "";
+    robot.respond(/describe instance ([-\a-zA-Z0-9]+)/i, function(msg) {
+        var instance = msg.match[1].toLowerCase(),
+            message  = "";
 
-		return new promise(function(resolve, reject) {
-			ec2.describeInstances({ InstanceIds : [ instance ]}, function(err, instance) {
-				if(err) {
-					reject(err);
-				}
+        return new promise(function(resolve, reject) {
+            ec2.describeInstances({ InstanceIds : [ instance ] }, function(err, instance) {
+                if(err) {
+                    reject(err);
+                }
 
-				resolve(instance);
-			});
-		}).then(function(instance) {
-			instance = instance.Reservations[0].Instances[0];
+                resolve(instance);
+            });
+        }).then(function(instance) {
+            instance = instance.Reservations[0].Instances[0];
 
-			message = message + instance.InstanceType + "\n";
-			message = message + instance.PublicIpAddress + "\n";
-			message = message + instance.PrivateIpAddress + "\n";
-
-			msg.send("```" + message + "```");
-		}).catch(function(e) {
-			msg.send("```" + e + "```");
-		});
-	});
+            message = message + "Instance type : " + instance.InstanceType + "\n";
+            message = message + "Public IP     : " + instance.PublicIpAddress + "\n";
+            message = message + "Private IP    : " + instance.PrivateIpAddress + "\n";
+            message = message + "Public DNS    : " + instance.PublicDnsName + "\n";
+            message = message + "Private DNS   : " + instance.PrivateDnsName + "\n";
+			message = message + "Launch time   : " + instance.LaunchTime + "\n";
+            
+            msg.send("```" + message + "```");
+        }).catch(function(e) {
+            msg.send("```" + e + "```");
+        });
+    });
 };
